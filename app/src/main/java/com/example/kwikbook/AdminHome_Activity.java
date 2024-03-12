4af895f4d5b897a0ec0d46912af240e2b13f7ce2
17_kowshik <kowshikeswarachaitanya@gmail.com>
2024-03-12 15:41:21 +0530

    Admin Home Commit

--- app/src/main/java/com/example/kwikbook/AdminHome_Activity.java
+++ app/src/main/java/com/example/kwikbook/AdminHome_Activity.java
@@ -1,24 +1,49 @@
 package com.example.kwikbook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AdminHome_Activity extends AppCompatActivity {
+    ImageView updateDB, lendingReports, penaltyInvoices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        updateDB = findViewById(R.id.updateDBid);
+        lendingReports = findViewById(R.id.lendingReportsid);
+        penaltyInvoices = findViewById(R.id.penaltyInvoicesid);
+
+        updateDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iUpdateDB = new Intent(AdminHome_Activity.this, updateDB_Activity.class);
+                startActivity(iUpdateDB);
+            }
         });
+
+        lendingReports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iLendingReports = new Intent(AdminHome_Activity.this, lendingReports_Activity.class);
+                startActivity(iLendingReports);
+            }
+        });
+
+        penaltyInvoices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iPenaltyInvoices = new Intent(AdminHome_Activity.this, penaltyInvoices_Activity.class);
+                startActivity(iPenaltyInvoices);
+            }
+        });
+
     }
 }
