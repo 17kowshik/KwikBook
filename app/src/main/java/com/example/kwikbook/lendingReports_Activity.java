83498a01c00abc31260568a02a75b3bbb5cb2f1b
MehereeshCh <mehereeshch@gmail.com>
2024-03-17 20:00:46 +0530

    XML Commit

--- app/src/main/java/com/example/kwikbook/lendingReports_Activity.java
+++ app/src/main/java/com/example/kwikbook/lendingReports_Activity.java
@@ -1,6 +1,8 @@
 package com.example.kwikbook;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class lendingReports_Activity extends AppCompatActivity {
+    ListView lv;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lending_reports);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        lv = findViewById(R.id.listView2);
+        et = findViewById(R.id.editTextText3);
     }
 }
--- app/src/main/java/com/example/kwikbook/penaltyInvoices_Activity.java
+++ app/src/main/java/com/example/kwikbook/penaltyInvoices_Activity.java
@@ -1,6 +1,7 @@
 package com.example.kwikbook;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +10,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class penaltyInvoices_Activity extends AppCompatActivity {
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_penalty_invoices);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        lv = findViewById(R.id.PenaltyInvoicesListView);
+
     }
 }
