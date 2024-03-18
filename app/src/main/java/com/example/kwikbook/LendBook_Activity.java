99893f6d197d6bd31c9379a9d7e9710719f26f3f
ADITYA <helloaadityaa369@gmail.com>
2024-03-18 12:18:08 +0530

    LendBook_Activity commit

--- app/src/main/java/com/example/kwikbook/LendBook_Activity.java
+++ app/src/main/java/com/example/kwikbook/LendBook_Activity.java
@@ -1,13 +1,20 @@
 package com.example.kwikbook;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class LendBook_Activity extends AppCompatActivity {
     EditText e;
@@ -30,7 +37,30 @@ public class LendBook_Activity extends AppCompatActivity {
             int userID=ldbHelper.getUserID(db, username);
             long bookID = Long.parseLong(e.getText().toString());
             if (ldbHelper.isAvailable(db, LendBook_Activity.this, bookID)){
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(LendBook_Activity.this);
+                alertDialogBuilder.setTitle("Confirm Book Lending");
+                Date today = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                String formattedDate = dateFormat.format(today);
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.DAY_OF_YEAR, 30);
+                SimpleDateFormat dateFormat30 = new SimpleDateFormat("dd-MM-yyyy");
+                String thirtyDaysFromNow = dateFormat30.format(calendar.getTime());
 
+                String lendingDetails = "<br>" +
+                        "<b>The Book you want to lend is :</b> " + ldbHelper.getBookName(db,bookID) + "<br>" +
+                        "<br>" +
+                        "<b>Lending Date :</b> " + "<i>" + formattedDate + "</i>" + "<br>" +
+                        "<br>" +
+                        "<b>Deadline to Return :</b> " + "<i>" + thirtyDaysFromNow + "</i>" + "<br>" +
+                        "<br>" +
+                        "<font color='red'><b><i>Note:</i></b> After the deadline, a 1 rupee fine is imposed for every day late.</font><br>" +
+                        "<br>";
+                alertDialogBuilder.setMessage(Html.fromHtml(lendingDetails));
+
+                alertDialogBuilder.setPositiveButton("CONFIRM LENDING", (dialogInterface, i) -> {
+                    ldbHelper.lendBook(db,LendBook_Activity.this,userID,bookID,formattedDate,thirtyDaysFromNow);
+                });
             }
         });
 
