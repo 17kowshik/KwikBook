75e8562a0b4c31da3e1b5406225fed277abdaf36
17_kowshik <kowshikeswarachaitanya@gmail.com>
2024-04-23 14:11:10 +0530

    Designs commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,12 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/example/kwikbook/MainActivity.java
+++ app/src/main/java/com/example/kwikbook/MainActivity.java
@@ -7,9 +7,10 @@ import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int SPLASH_TIME_OUT = 2000;
+    private static final int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView img = findViewById(R.id.splash_logo);
+        int gifResource = R.drawable.kb_logo_gif;
+
+        Glide.with(this)
+                .asGif()
+                .load(gifResource)
+                .into(img);
         ScaleAnimation sA = new ScaleAnimation(
-                0.5f, 1.0f,
-                0.5f, 1.0f,
+                0.5f, 2.0f,
+                0.5f, 2.0f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f
         );
